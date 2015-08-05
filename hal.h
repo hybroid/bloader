f8a865c9010acfbad8444e94c8a2f4d1afae2017
hybroid <none@none>
2015-08-05 10:26:02 +0300

    Add hal.h for device dependings

---
+++ hal.h
@@ -0,0 +1,15 @@
+/*
+ * hal.h
+ *
+ *  Created on: 05 авг. 2015 г.
+ *      Author: Tony Darko
+ */
+
+#ifndef HAL_H_INCLUDED
+#define HAL_H_INCLUDED
+
+#include <avr/io.h>
+
+//
+
+#endif /* HAL_H_INCLUDED */
